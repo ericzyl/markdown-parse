b68be158059d6601d4eb4b467d31f5e93fa7bf53
ericzyl <87045758+ericzyl@users.noreply.github.com>
2022-01-27 20:00:58 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,9 @@
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,4 +11,26 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void testGetLinksOne() throws IOException{
+        String contents = Files.readString(Path.of(".;/test-file.md"));
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        assertEquals(expected, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testGetLinksTwo() throws IOException{
+            String contents = Files.readString(Path.of("../test-file.md"));
+            List<String> expected = List.of("https://something.com", "some-page.html");
+            assertEquals(expected, contents);
+    }
+
+    @Test
+    public void testGetLinksThree() throws IOException{
+            String contents = Files.readString(Path.of("../test-file.md"));
+            List<String> expected = List.of("https://something.com", "some-page.html");
+            assertEquals(expected, contents);
+    }
+ 
 }
