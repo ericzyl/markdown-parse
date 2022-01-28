d34c29ea1ea8ce3ef1b6dbcff5d6f1c51eef5d64
ericzyl <87045758+ericzyl@users.noreply.github.com>
2022-01-28 09:04:23 -0800

    junit

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,21 +14,21 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinksOne() throws IOException{
-        String contents = Files.readString(Path.of(".;/test-file.md"));
+        String contents = Files.readString(Path.of("./test-file.md"));
         List<String> expected = List.of("https://something.com", "some-page.html");
         assertEquals(expected, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testGetLinksTwo() throws IOException{
-            String contents = Files.readString(Path.of("../test-file.md"));
+            String contents = Files.readString(Path.of("./test-file.md"));
             List<String> expected = List.of("https://something.com", "some-page.html");
             assertEquals(expected, contents);
     }
 
     @Test
     public void testGetLinksThree() throws IOException{
-            String contents = Files.readString(Path.of("../test-file.md"));
+            String contents = Files.readString(Path.of("./test-file.md"));
             List<String> expected = List.of("https://something.com", "some-page.html");
             assertEquals(expected, contents);
     }
