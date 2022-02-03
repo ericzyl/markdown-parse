101f5ab8fca69018e83d38e9cc29eefb6fedf2f8
ericzyl <87045758+ericzyl@users.noreply.github.com>
2022-02-03 10:03:50 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.junit.*;
@@ -20,36 +21,45 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinksOne() throws IOException{
-        try{
-            String contents = Files.readString(Path.of("./test-file.md"));
-            List<String> expected = List.of("https://something.com", "some-page.html");
-            assertEquals(expected, MarkdownParse.getLinks(contents));
-        }catch(NoSuchFileException e){
-            
-        }
-
+        String filename = "test-file.md";
+        //To get path of file
+        Path path = Paths.get(filename);
+        //To print absolute path of file
+        String contents = Files.readString(path);
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        assertEquals(expected, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testGetLinksTwo() throws IOException{
-        try{
-            String contents = Files.readString(Path.of("./test-file.md"));
-            List<String> expected = List.of("https://something.com", "some-page.html");
-            assertEquals(expected, MarkdownParse.getLinks(contents));
-        }catch(NoSuchFileException e){
-            
-        }
+        String filename = "test-file.md";
+        //To get path of file
+        Path path = Paths.get(filename);
+        //To print absolute path of file
+        String contents = Files.readString(path);
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        assertEquals(expected, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testGetLinksThree() throws IOException{
-        try{
+        String filename = "test-file.md";
+        //To get path of file
+        Path path = Paths.get(filename);
+        //To print absolute path of file
+        String contents = Files.readString(path);
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        assertEquals(expected, MarkdownParse.getLinks(contents));
+    }
+ 
+}
+
+/**
+ * try{
             String contents = Files.readString(Path.of("./test-file.md"));
             List<String> expected = List.of("https://something.com", "some-page.html");
             assertEquals(expected, MarkdownParse.getLinks(contents));
         }catch(NoSuchFileException e){
             
         }
-    }
- 
-}
+ */
